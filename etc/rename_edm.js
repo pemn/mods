b5900bafcd4f3486356e96bec60b5c02b536f3e8
Paulo Ernesto <pemn@users.noreply.github.com>
2019-10-31 09:01:49 -0300

    Update rename_edm.js

--- etc/rename_edm.js
+++ etc/rename_edm.js
@@ -1,7 +1,7 @@
 //#!cscript
 
 WScript.Echo("rename_edm");
-var re1 = new RegExp(/(.+)\((.+)-(.+)-(.+)\)\.abc/i);
+var re1 = new RegExp(/(.+)\((\d+).*\.(.+)\.(.+)\)/i);
 
 // WScript.Echo(re.exec('abc'));
 
@@ -9,13 +9,20 @@ var WshShell = WScript.CreateObject("WScript.Shell");
 var WshSysEnv = WshShell.Environment("Process");
 
 fso = WScript.CreateObject("Scripting.FileSystemObject");
-fc = fso.GetFolder(".").Files;
+fc = fso.GetFolder(".").SubFolders;
 for(var objEnum = new Enumerator(fc); !objEnum.atEnd(); objEnum.moveNext()) {
   var name = objEnum.item().Name;
   // WScript.Echo(name);
   var m = re1.exec(name);
   if (m) {
+    WScript.Echo("");
     WScript.Echo(m);
-    fso.MoveFile(objEnum.item().Name, "tmp_" + objEnum.item().Name);
+    var rename = m[4] + " - " + m[3] + m[2] + " - " + m[1]
+    if (rename.substr(0,2) != "20") {
+      rename = '20' + rename;
+    }
+    WScript.Echo(rename);
+    objEnum.item().Move(rename);
+    //fso.MoveFile(objEnum.item().Name, "tmp_" + objEnum.item().Name);
   }
 }
