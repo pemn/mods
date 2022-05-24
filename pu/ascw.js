e619ac304d9cdf165ed7cdd51d959c08b9d656dc
Paulo Ernesto <pemn@users.noreply.github.com>
2022-05-24 08:25:09 -0300

    Update ascw.js

--- pu/ascw.js
+++ pu/ascw.js
@@ -1,9 +1,9 @@
 // apex shipping contract watcher
 var ASCW = {
-	active: true, n: false, hash: [],
-	talk: function(text) {
-		speechSynthesis.speak(new SpeechSynthesisUtterance(text));
-	},
+    active: true, n: false, hash: [],
+    talk: function(text) {
+        speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+    },
     look: function(text) {
         if (! this.hash.includes(text)) {
             this.hash.push(text);
@@ -15,23 +15,23 @@ var ASCW = {
             this.n = false;
         }
     },
-	loop: function() {
-		nl = document.querySelectorAll("div[title^='Rating'] + div");
-		if (nl.length) {
+    loop: function() {
+        nl = document.querySelectorAll("div[title^='Rating'] + div");
+        if (nl.length) {
             for (let i=0;i < nl.length; i++) {
                 this.look(nl[i].innerText);
             }
-		}
+        }
         this.n = true;
-		if (this.active) {
-			setTimeout(this.loop.bind(this), 9999);
-		} else {
-			this.talk("beep");
+        if (this.active) {
+            setTimeout(this.loop.bind(this), 9999);
+        } else {
+            this.talk("beep");
             this.n = false;
             this.active = true;
             this.hash.length = 0;
-		}
-	},
+        }
+    },
     stop: function() {
         this.active = false;
     }
